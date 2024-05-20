bef762fb28e7c617a18821ab396c5bbd52db606a
«Aqeedat-Nabi» <2023_aqeedat.ghulam@iticbcn.cat>
2024-05-20 20:17:08 +0200

    final working odoo modules for TIC Hospital

--- odoo/odoo-custom-addons/invoicing/models/models.py
+++ odoo/odoo-custom-addons/invoicing/models/models.py
@@ -7,9 +7,8 @@ class Invoicing(models.Model):
     _name = 'invoicing.invoicing'
     _description = 'Invoices'
 
-    name = fields.Char()
+    customer_invoice = fields.Char()
     value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
     customer_id = fields.Many2one('res.partner', string='Customer')
     expiration_date = fields.Date(string='Expiration Date')
@@ -24,6 +23,4 @@ class Invoicing(models.Model):
 
     @api.depends('value')
     def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
-
+        pass
